9e3199033a3e5de5f1def1600f72c49b58ede10e
Brian Burrell <bburrell.brian@gmail.com>
2015-04-12 07:14:56 -0600

    release

--- release.sh
+++ release.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir -p release/share-coin
 cd release
-../configure --libdir=/src/share-coin/release/share-coin --bindir=/src/share-coin/release/share-coin --sbindir=/src/share-coin/release/share-coin --docdir=/src/share-coin/release/share-coin --with-libshare=/h/src/sharelib/build
+../configure --libdir=/src/share-coin/release/share-coin --bindir=/src/share-coin/release/share-coin --sbindir=/src/share-coin/release/share-coin --docdir=/src/share-coin/release/share-coin --with-libshare=/src/sharelib/release
 make
 make install
 tar -cpf sharecoin-`arch`.tar share-coin
